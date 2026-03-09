274c54c9baccf6c53043cb48e3630d931eb5e0f9
Wisit Somsorod <crisiswisit@gmail.com>
2026-03-10 06:39:42 +0700

    morning 20260310

--- src/llm_architect/Embedding.py
+++ src/llm_architect/Embedding.py
@@ -4,26 +4,19 @@ import numpy.typing as npt
 
 class Embedding:
 
-    embeddingDict: npt.NDArray[np.float64]
+    embeddingDict: List[npt.NDArray[np.float64]]
     d_model: int
 
-    def __init__(self, d_model, embeddingDict):
+    def __init__(self, d_model: int, embeddingDict: List[npt.NDArray[np.float64]]):
         self.embeddingDict = embeddingDict
         self.d_model = d_model
 
-    def embed(self, words: List[str]) :
+    def embed(self, wordIndex: int) :
 
-        embededText = np.zeros((len(words), self.d_model), dtype=np.float64)
-        nullEmbed = np.zeros((0, self.d_model), dtype=np.float64)
-
-        for i in range(len(words)):
-            isContent = any(words[i] == dictWord for dictWord in self.embeddingDict.keys())
-            if isContent :
-                embededText[i][:] = self.embeddingDict[words[i]]
-            else :
-                embededText[i][:] = nullEmbed
-
-        return np.array(embededText)
-    
-    def deEmbed(self, embeddedText: npt.NDArray[np.float64]):
+        if (wordIndex > len(self.embeddingDict) - 1) :
+            raise Exception("word index > size of embeddict")
         
+        return self.embeddingDict[wordIndex]
+    
+
+    def backward(self, )
